26fbc98e7d40eaac712e8abfa176260c3fd3f55a
AmeyaKulkarni2001 <71250891+AmeyaKulkarni2001@users.noreply.github.com>
2021-01-23 18:57:46 +0530

    Update Prime Factorization Of A Number.java

--- Prime Factorization Of A Number.java
+++ Prime Factorization Of A Number.java
@@ -3,18 +3,17 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-    
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        
-        for(int div = 2; div*div <= n; div++){
-            while(n % div == 0){
-                n = n/div;
-                System.out.println(div + " ");
+
+        for (int div = 2; div * div <= n; div++) {
+            while (n % div == 0) {
+                System.out.print(div + " ");
+                n = n / div;
             }
         }
-        if(n != 1){
-            System.out.println(n);
-        }
+
+        if (n != 1)
+            System.out.print(n);
     }
 }
